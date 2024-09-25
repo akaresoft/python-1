b8dce43504828c1a4846cde283e61c4e7c9666b2
Muslim HODJA <85079865+mslmhodja@users.noreply.github.com>
2024-09-25 15:38:02 +0300

    Create uygulama-4.py

---
+++ uygulama-4.py
@@ -0,0 +1,12 @@
+# verilen 4 basamaklı sayının basamaklarındaki rakamları bulan kodu yazınız
+
+# a=5879  
+# a4 = int (a /1000)) binler basamağını verir
+# a3 =                yüzler basamağını verir
+# a2 =                onlar basamağını verir
+# a3 =                birler basamağını verir
+
+# 1
+# 4
+# 5
+# 8
