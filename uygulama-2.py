d23b9507631192225b2e7949ffd34b04db4c8154
Muslim HODJA <85079865+mslmhodja@users.noreply.github.com>
2024-09-25 14:42:49 +0300

    Create uygulama-2.py

---
+++ uygulama-2.py
@@ -0,0 +1,14 @@
+# aşağıdaki ifaeyi çıktı verne kodu yazınız
+# her kelime yarı değişkende tutulacak
+# her satırı ayrı print ile yazıdr
+
+# Biz BuGün
+# Python Değişkenleri
+# Kurallarını
+# İşledik
+k1,k2,k3,k4,k5,k6,k7 = "Biz", "Bu", "Gün" ,"Python", "Değişkenleri", "Kurallarını", "İşledik"
+
+print(k1,k2+k3)
+print(k4,k5)
+print(k6)
+print(k7)
