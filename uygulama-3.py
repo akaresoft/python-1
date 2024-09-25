1e2178058a298eee382feccf8e088738957dd471
Muslim HODJA <85079865+mslmhodja@users.noreply.github.com>
2024-09-25 15:07:55 +0300

    Create uygulama-3.py

---
+++ uygulama-3.py
@@ -0,0 +1,8 @@
+# iki yazılı iki sözlü notunu tutan ve 
+# bunların ortalamasını hesaplayan 
+# ve ekrana yazan program
+y1,y2,s1,s2 = 25,55,90,80
+
+ort = (y1+y2+s1+s2) / 4
+
+print("Ortalama=", ort)
