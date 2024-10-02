e96062451a3c11e4d957f8678e446ac17af1b08f
Muslim HODJA <85079865+mslmhodja@users.noreply.github.com>
2024-10-02 13:29:08 +0300

    Create uygulama-6.py

---
+++ uygulama-6.py
@@ -0,0 +1,25 @@
+# rastgele 4 basamaklı sayının basamaklarındaki rakamları bulan kodu yazınız
+
+# a=5879 rastgele  
+# a4 = int (a /1000)) binler basamağını verir
+# a3 =                yüzler basamağını verir
+# a2 =                onlar basamağını verir
+# a3 =                birler basamağını verir
+
+# 1
+# 4
+# 5
+# 8
+import random
+Sayi = random.randrange(1000, 10000)
+print(Sayi)
+m=str(Sayi)
+b4 = m[0]
+b3 = m[1]
+b2 = m[2]
+b1 = m[3]
+
+print(b4)
+print(b3)
+print(b2)
+print(b1)
