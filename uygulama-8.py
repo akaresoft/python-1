91f08e310a43de5479632adb7925aecf3360eb18
Muslim HODJA <85079865+mslmhodja@users.noreply.github.com>
2024-10-02 13:39:56 +0300

    Create uygulama-8.py

---
+++ uygulama-8.py
@@ -0,0 +1,6 @@
+txt = "To check if a certain phrase or character is present in a string, we can use the keyword in!"
+word = "Can"
+if word in txt:
+  print("Evet '"+ word+"' kelimesi var.")
+else:
+  print("Hayır '"+ word+"' kelimesi yok.")
