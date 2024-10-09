59ab90a24084e5bb4cea1d74bde669f5b55653f2
Muslim HODJA <85079865+mslmhodja@users.noreply.github.com>
2024-10-09 14:30:09 +0300

    Create uygulama-14.py

---
+++ uygulama-14.py
@@ -0,0 +1,6 @@
+ogrenciler = ["ali","ahmet","şule","kasım","ferhat","eda","kerem","ahmet","furkan","mehmet","hasan","eda","lale","ufuk","ece","selin","melis","derya","tarık"]
+
+for ogr in ogrenciler:
+  print(ogr)
+
+
