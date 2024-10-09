654fc29ae063c667585a4ad878e54a1e5f850b46
Muslim HODJA <85079865+mslmhodja@users.noreply.github.com>
2024-10-09 13:30:56 +0300

    Create uygulama-13.py

---
+++ uygulama-13.py
@@ -0,0 +1,7 @@
+# 500 den 1000 e kadar 50şer artan 
+# 800-900 arası hariç (800 dahil)
+# sayıları ekrana yazan program (50p)
+
+for s in range(500, 1001,50):
+  if not (s>800 and s<=900):
+     print(s)
