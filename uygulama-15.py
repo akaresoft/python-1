ece61809f1eed9489c9460a77f7c89678bbba30a
Muslim HODJA <85079865+mslmhodja@users.noreply.github.com>
2024-10-09 14:42:07 +0300

    Create uygulama-15.py

---
+++ uygulama-15.py
@@ -0,0 +1,10 @@
+# oğrencinin 6 notunu diziye aktar
+# 50 60 40 90 45 85
+# dizideki notların toplamını bul ve ekrana yaz
+# dizideki notların ortalamasını bul ve ekrana yaz
+# 3. notu 100 olarak değiştir # 50 60 100 90 45 85
+# 1. notu sil # 60 100 90 45 85
+# dizinin sonuna 90 notunu ekle # 60 100 90 45 85 90
+# dizinin 4 elamanına 20 notunu ekle # 60 100 90 20 45 85 90
+# dizinin ilk elamanını sil  # 100 90 20 45 85 90
+# ortalamayı tekrar hesapla 
