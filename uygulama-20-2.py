99198dc3ee74b7fe18f4e0434a6a5c78a9f641fe
Akaresoft Yazılım <58430984+akaresoft@users.noreply.github.com>
2024-11-20 12:38:16 +0300

    Create uygulama-20-2.py

---
+++ uygulama-20-2.py
@@ -0,0 +1,22 @@
+# okul 
+#	okulno, adı, ili, ilçesi, öğrenci sayısı, öğretmen sayısı, türü, kuruluş yılı
+    
+okul = {
+  "okulno":1,
+  "adi" : "Asım Kocabıyık MTAL",
+  "ili":"İstanbul",
+  "ilcesi":"Esenyurt",
+  "ogrencisayisi":875,
+  "ogretmensayisi":63,
+  "türü":"Anadolu Teknik ve Meslek Lisei",
+  "kurulus":1991
+}
+
+print ("Okul No" , okul["okulno"])
+print ("Okul Adı: " , okul["adi"])
+print ("Okulun İli", okul["ili"])
+print ("Okulun İlcesi", okul["ilcesi"])
+print ("Okulun Ögrenci Sayisi", okul["ogrencisayisi"])
+print ("Okulun Ögretmen Sayisi", okul["ogretmensayisi"])
+print ("Okulun türü", okul["türü"])
+print ("Okulun kurulus Yılı", okul["kurulus"])
