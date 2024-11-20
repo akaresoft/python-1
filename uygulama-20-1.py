dd9d9b62fb4fa4911db6aba1e84ced1ede435daa
Akaresoft Yazılım <58430984+akaresoft@users.noreply.github.com>
2024-11-20 12:12:34 +0300

    Create uygulama-20-1.py

---
+++ uygulama-20-1.py
@@ -0,0 +1,15 @@
+
+agac = {
+  "numara":1,
+  "tur" : "Meşe",
+  "boyu":270,
+  "yas":7,
+  "bolge":"Belgrad-1"
+}
+
+print ("Ağaç Numarası: ", agac["numara"])
+print ("Ağaç Türü: ", agac["tur"])
+print ("Ağaç Boyu: ", agac["boyu"],"cm")
+print ("Ağaç Yaşı: ", agac["yas"])
+print ("Ağacın Bulunduğu Bölge: ", agac["bolge"])
+
